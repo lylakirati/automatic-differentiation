8b7c4fd22b1e3773702a11b8f96b0cb43ab9ee01
Lyla Kiratiwudhikul <lkiratiwudhikul@fas.harvard.edu>
2022-11-10 23:51:59 -0500

    listed out dualnum structure + methods

--- src/team20ad/dualNumber.py
+++ src/team20ad/dualNumber.py
@@ -9,3 +9,46 @@ class DualNumber:
 	def __str__(self):
 		pass
 
+	def __neg__(self):
+		pass
+
+	def __add__(self, other):
+		pass
+
+	def __radd__(self, other):
+		pass
+
+	def __sub__(self, other):
+		pass
+
+	def __rsub__(self, other):
+		pass
+
+	def __mul__(self, other):
+		pass
+
+	def __rmul__(self, other):
+		pass
+
+	def __truediv__(self, other):
+		pass
+
+	def __rtruediv__(self, other):
+		pass
+
+	def __pow__(self, p):
+		pass
+
+	def __rpow__(self, other):
+		pass
+
+	def __eq__(self, other):
+		pass
+
+	def __ne__(self, other):
+		pass
+
+
+
+
+
